ce9f08b1f7eb80b386e19d924053356158217dec
Alejandro Saenz <asaenzestrada@gmail.com>
2014-12-08 18:59:06 -0800

    Release Jeopardy ICC 1.0

--- Game.py
+++ Game.py
@@ -1,10 +1,14 @@
 import easygui as eg
 import functions
+from sys import exit
+
 
 class Game:
     questions = functions.Functions.handleJson('Assets/opciones.json')
-    many = 10
     status = True
+    ErrorLimit = 5
+    GameLimit = 30
+    MoneyLimit = -1000
 
     def gameflow(player):
         while Game.status is True:
@@ -13,16 +17,8 @@ class Game:
 
             for category in Game.questions:
                 cats.append(category)
-
-            msg = "Juego de: " + player.name
-            msg += "\nCreditos: " + str(player.money)
-            msg += "\n\nRonda: " + str(player.lap)
-            msg += "\n"
-            msg += "\nAciertos: " + str(player.right)
-            msg += "\nErrores: " + str(player.mistakes)
-
             title = "Jeopardy SuperCool de ICC"
-            category = eg.choicebox(msg, title, cats)
+            category = eg.choicebox(Game.getMessage(player), title, cats)
             if not category:
                 break
             choices = list()
@@ -56,16 +52,22 @@ class Game:
                         player.mistakes += 1
                         player.money -= 100
 
-                if player.mistakes > 5 or player.money < 0:
+                if player.mistakes > Game.ErrorLimit or player.money < Game.MoneyLimit:
                     Game.status = False
+                    eg.msgbox("Has perdido, este jeopardy es mucho para ti..." + msg1, ok_button="byeeeee :(")
+                    exit()
+                if player.lap > Game.GameLimit:
+                    Game.status = False
+                    Win = True
+        if Win:
+            eg.msgbox("Has Ganado, te mereces el respeto de un pythonista...\ny te llevas $"
+                      + str(player.money) + "\n" + Game.getMessage(player), ok_button="Salir")
 
-        msg = "\nJuego de: " + player.name
-        msg += "\nCreditos: " + str(player.money)
-        msg += "\n\nRonda: " + str(player.lap)
-        msg += "\n"
-        msg += "\nAciertos: " + str(player.right)
-        msg += "\nErrores: " + str(player.mistakes)
-
-        if Game.status == False:
-            eg.msgbox("Has perdido, este jeopardy es mucho para ti..." + msg, ok_button="byeeeee :(");
-        eg.msgbox("Has Ganado, te mereces el respeto de un pythonista..." + msg, ok_button="Salir");
+    def getMessage(player):
+        msg1 = "Juego de: " + player.name
+        msg1 += "\nCreditos: " + str(player.money)
+        msg1 += "\n\nRonda: " + str(player.lap)
+        msg1 += "\n"
+        msg1 += "\nAciertos: " + str(player.right)
+        msg1 += "\nErrores: " + str(player.mistakes)
+        return  msg1
